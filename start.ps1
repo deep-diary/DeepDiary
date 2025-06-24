14b6e59b74d2e8fac7c2764437b3c03a71e12727
blue.ge <blue.ge@smart.com>
2025-06-24 08:54:54 +0800

    Remove start.ps1 from tracking

--- start.ps1
+++
@@ -1,9 +0,0 @@
-# Switch to the DeepWin directory
-Set-Location -Path ".\DeepWin"
-
-# Activate the conda environment
-conda activate DiaryWin
-
-# Display the current directory and environment information
-Write-Host "Current directory: $(Get-Location)"
-Write-Host "Current conda environment: DiaryWin" 
